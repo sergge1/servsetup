ec929e94b8ab0e538f719a48e3740289fc32f605
sergge1 <shvozdyk@gmail.com>
2021-01-31 12:57:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker pi
 
 sudo pip3 install docker-compose
+
+#ввести дані авторизації git
+git config --global user.name sergge1
+git config --global user.email 
+#зчитати коректність записаних даних
+git config --list
