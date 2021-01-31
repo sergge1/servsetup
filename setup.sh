d1e6d9ef6ad835f0b950b2e34712721642e813f9
sergge1 <shvozdyk@gmail.com>
2021-01-31 13:24:53 +0200

    torrserver

--- setup.sh
+++ setup.sh
@@ -11,3 +11,6 @@ git config --global user.name sergge1
 git config --global user.email 
 #зчитати коректність записаних даних
 git config --list
+
+#переміщуємо докер-компоус.ямл в робочу папку
+mv /home/pi/servsetup/docker-compose.yaml /home/pi/docker-compose.yaml
